020b7c08c95f7f227a0e28d0569a5a8620a051de
quinnnned <quinnnned@gmail.com>
2017-11-12 09:53:20 -0600

    Form Basic Project Structure
    * Using create react app, for two reasons:
      1. I will probably eventually build a playable web interface
      2. I don't feel like setting up a test framework right now

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {View} from './view'
+
+ReactDOM.render(
+    <View />, 
+    document.getElementById('root')
+)
---
+++ src/view.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const View = () => 
+    <span>
+        Hello World!
+    </span>
